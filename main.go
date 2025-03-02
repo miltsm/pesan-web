b97536596f194b20cadd393535235b9aba0de3cb
izzatsyamil <izzat@atomnode.com>
2025-03-02 17:15:18 +0800

    Updated main.go according docker setup

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"embed"
 	"fmt"
+	"os"
+	"strconv"
 
 	"log"
 	"net/http"
@@ -12,14 +14,19 @@ import (
 var staticFs embed.FS
 
 func main() {
+	port, err := strconv.ParseInt(os.Getenv("PORT"), 10, 32)
+	if err != nil {
+		fmt.Printf("[WARN] %v\n", err)
+		port = 3000
+	}
 	router := http.NewServeMux()
 	router.Handle("/", http.FileServerFS(staticFs))
 	srv := http.Server{
-		Addr:    ":3000",
+		Addr:    fmt.Sprintf("%s:%d", os.Getenv("HOST"), port),
 		Handler: router,
 	}
 	fmt.Printf("listening to port 3000..\n")
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatalf("[FATAL] %v", err)
 	}
