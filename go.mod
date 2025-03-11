fa95915232b6cba011f355013ae2f8f51c030cef
izzatsyamil <izzat@atomnode.com>
2025-03-11 12:38:18 +0800

    Moved session handling(mostly) back to core server; web only handle verification part

--- go.mod
+++ go.mod
@@ -4,17 +4,17 @@ go 1.24.0
 
 require (
 	github.com/go-webauthn/webauthn v0.12.1
-	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v5 v5.7.2
 	github.com/redis/go-redis/v9 v9.7.1
 )
 
 require (
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
 	github.com/go-webauthn/x v0.1.18 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/google/go-tpm v0.9.3 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"crypto/tls"
-	"database/sql"
+	"time"
+
 	"embed"
+	"encoding/json"
 	"fmt"
 
 	"os"
@@ -12,24 +14,33 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/go-webauthn/webauthn/webauthn"
+	"github.com/google/uuid"
 	_ "github.com/jackc/pgx/v5/stdlib"
-	"github.com/miltsm/pesan-web/middleware"
-	"github.com/miltsm/pesan-web/session"
+
 	"github.com/redis/go-redis/v9"
 )
 
+// NOTE: web - APPLICATION
+// *this is for an nvim easy jump; consider it as table of contents
+
 //go:embed .well-known
 var wellKnownFS embed.FS
+
+var (
+	cache   *redis.Client
+	wbAuthn *webauthn.WebAuthn
+)
 
 func main() {
 	var certPem []byte
 	keyPem, err := os.ReadFile(os.Getenv("WEB_PRIVATE_KEY_PATH"))
 	if err != nil {
-		fmt.Printf("invalid private key: %v\n", err)
+		fmt.Printf("[FATAL] invalid private key: %v\n", err)
 	}
 	certPem, err = os.ReadFile(os.Getenv("WEB_CERT_PATH"))
 	if err != nil {
-		fmt.Printf("invalid cert: %v\n", err)
+		fmt.Printf("[FATAL] invalid cert: %v\n", err)
 	}
 	var certTls tls.Certificate
 	certTls, err = tls.X509KeyPair(certPem, keyPem)
@@ -40,22 +51,24 @@ func main() {
 		fmt.Printf("[WARN] %v\n", err)
 		port = 3000
 	}
-	db := setupDb()
-	cache := setupCache()
+
+	establishCache()
+	configureWebAuthn()
+
 	router := http.NewServeMux()
 	// NOTE: serve .well-known file
 	router.Handle("/", http.FileServerFS(wellKnownFS))
-	sessionHandler := session.New(db, *cache)
+
 	// NOTE: endpoints
-	router.HandleFunc("POST /public-key/assert/challenge", sessionHandler.PostRequestAssertation)
-	router.HandleFunc("POST /public-key/assert/{user_handle}", sessionHandler.PostAssertPublicKey)
-	router.HandleFunc("POST /public-key/attest/discover", sessionHandler.PostPublicKeyAssertDiscover)
+	router.HandleFunc("POST /public-key/assert/{challenge}", verifyAssertation)
+
 	srv := http.Server{
 		Addr:      fmt.Sprintf("%s:%d", os.Getenv("HOST"), port),
-		Handler:   middleware.Logger(router),
+		Handler:   logger(router),
 		TLSConfig: cfg,
 	}
-	fmt.Printf("listening to port %s..\n", srv.Addr)
+	fmt.Printf("[INFO] listening to port %s..\n", srv.Addr)
+
 	//err = srv.ListenAndServeTLS("", "")
 	err = srv.ListenAndServe()
 	if err != nil {
@@ -63,29 +76,53 @@ func main() {
 	}
 }
 
-func setupDb() *sql.DB {
-	port, err := strconv.ParseInt(os.Getenv("POSTGRES_PORT"), 10, 32)
-	if err != nil {
-		fmt.Printf("[WARN] %v\n", err)
-		port = 5432
-	}
-	var pwd []byte
-	pwd, err = os.ReadFile(os.Getenv("POSTGRES_PASSWORD_FILE"))
-	if err != nil {
-		log.Fatalf("[ERROR] %v\n", err)
-	}
-	pgUser, pgHost, pgDb := os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_DB")
-	dsn := fmt.Sprintf("postgres://%s:%s@%s:%d/%s", pgUser, pwd, pgHost, port, pgDb)
-	var db *sql.DB
-	db, err = sql.Open("pgx", dsn)
-	if err != nil {
-		log.Fatalf("[ERROR] unable to use data source name -\n%v\n", err)
-	}
-	fmt.Println("db connected!")
-	return db
+// NOTE: web - MIDDLEWARES
+// NOTE: web/middlewares/logger
+type wrappedWriter struct {
+	http.ResponseWriter
+	statusCode int
 }
 
-func setupCache() *redis.Client {
+func (w *wrappedWriter) WriteHeader(statusCode int) {
+	w.ResponseWriter.WriteHeader(statusCode)
+	w.statusCode = statusCode
+}
+
+func logger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		wrapped := &wrappedWriter{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+		next.ServeHTTP(wrapped, r)
+		log.Printf("[INFO]\t%d\t%s\t%s\t%s", wrapped.statusCode, r.Method, r.RequestURI, time.Since(start))
+	})
+}
+
+// NOTE: web - WEBAUTHN
+func configureWebAuthn() {
+	host, androidOrigin := os.Getenv("WBAUTHN_RP_ID"), fmt.Sprintf("android:apk-key-hash:%s", os.Getenv("ANDROID_KEY_HASH"))
+	origin := fmt.Sprintf("%s://%s", os.Getenv("WEB_SCHEME"), host)
+
+	config := &webauthn.Config{
+		RPDisplayName: "Pesan authentication",
+		RPID:          host,
+		RPOrigins:     []string{origin, androidOrigin},
+	}
+
+	var err error
+	wbAuthn, err = webauthn.New(config)
+	if err != nil {
+		log.Fatalf("[FATAL] %v", err)
+		return
+	}
+
+	fmt.Println("[INFO] webauthn configured!")
+}
+
+// NOTE: web - CACHE
+func establishCache() {
 	rdHost, rdPort := os.Getenv("RDS_HOST"), os.Getenv("RDS_PORT")
 	if len(rdHost) == 0 {
 		fmt.Println("[WARN] redis host isn't specified!")
@@ -96,10 +133,82 @@ func setupCache() *redis.Client {
 		fmt.Println("[WARN] redis port isn't specified!")
 		port = 6379
 	}
-	return redis.NewClient(&redis.Options{
+	cache = redis.NewClient(&redis.Options{
 		Addr:     fmt.Sprintf("%s:%d", rdHost, port),
 		Password: "",
 		DB:       0,
 		Protocol: 2,
 	})
+
+	fmt.Println("[INFO] cache established!")
+}
+
+// NOTE: web - SESSIONS
+type User struct {
+	Id          uuid.UUID             `json:"id"`
+	UserHandle  string                `json:"user_handle"`
+	DisplayName string                `json:"display_name"`
+	Credentials []webauthn.Credential `json:"credentials"`
+}
+
+func (u *User) WebAuthnID() []byte {
+	return []byte(u.Id.String())
+}
+
+func (u *User) WebAuthnName() string {
+	return u.UserHandle
+}
+
+func (u *User) WebAuthnDisplayName() string {
+	return u.DisplayName
+}
+
+func (u *User) WebAuthnCredentials() []webauthn.Credential {
+	return u.Credentials
+}
+
+// NOTE: web - ASSERT
+func verifyAssertation(w http.ResponseWriter, r *http.Request) {
+
+	challengeId := r.PathValue("challenge")
+	cacheKey := fmt.Sprintf("asserts:%s", challengeId)
+
+	ctx := r.Context()
+
+	cachedUser, err := cache.JSONGet(ctx, cacheKey, ".user").Result()
+
+	var cacheSession string
+	cacheSession, err = cache.JSONGet(ctx, cacheKey, ".session").Result()
+	if err != nil {
+		http.Error(w, "[ERROR] WEB - session timeout", http.StatusRequestTimeout)
+		return
+	}
+
+	var user User
+	err = json.Unmarshal([]byte(cachedUser), &user)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("[ERROR] WEB - %v", err), http.StatusFailedDependency)
+		cache.JSONClear(ctx, cacheKey, "$")
+		return
+	}
+
+	var session webauthn.SessionData
+	err = json.Unmarshal([]byte(cacheSession), &session)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("[ERROR] WEB - %v", err), http.StatusFailedDependency)
+		cache.JSONClear(ctx, cacheKey, "$")
+		return
+	}
+
+	_, err = wbAuthn.FinishRegistration(&user, session, r)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("[ERROR] WEB - %v", err), http.StatusInternalServerError)
+		cache.JSONClear(ctx, cacheKey, "$")
+		return
+	}
+
+	// NOTE: slightly extend for the case of legit verification but at the verge of session ending
+	cache.ExpireAt(ctx, cacheKey, time.Now().Add(30*time.Second)).Result()
+
+	w.WriteHeader(http.StatusAccepted)
 }
